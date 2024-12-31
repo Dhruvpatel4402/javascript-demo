cdebd0a2c43e6e34ac6a0f1d483ed2b94d48a656
Dhruvpatel4402 <133862279+Dhruvpatel4402@users.noreply.github.com>
2024-12-31 06:14:10 +0000

    stack and heap

---
+++ 01_basic/stackandheap.js
@@ -0,0 +1,24 @@
+// stack (premetive),heap(non premitive)
+
+let myyoutubename="dhruvpatel"
+
+let anothername=myyoutubename
+anothername="Dhruvcoder"
+console.log(anothername);
+console.log(myyoutubename);
+
+
+let userOne={
+    email:"Dhruv@gmail.com",
+    upi:"dhruv@sbi"
+
+
+}
+
+let userTw0=userOne
+
+userTw0.email="patel@gmail.com"
+
+console.log(userOne.email);
+console.log(userTw0.email);
+
